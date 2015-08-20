0863877a6ae4c0dc4cfe1c9c37cd6ce1d9f13ef8
jwareing <jwareing89@gmail.com>
2015-08-20 16:43:17 -0700

    added first work on lik list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newTail = Node(value);
+    if (this.tail === null){
+      this.tail = newTail;
+      this.head = newTail;
+    }
+    else{
+      this.tail.next = newTail;
+      this.tail = newTail;
+    };
   };
 
   list.removeHead = function(){
+    var oldHead = this.head;
+    if (this.head !== null){
+      this.head = this.head.next;
+    }
+    return oldHead.value;
   };
 
   list.contains = function(target){
+    if (this.head.value === target){
+      
+    }
   };
 
   return list;
