da0fe8857ab5a2115ee43d913dc0946853d97dc1
jwareing <jwareing89@gmail.com>
2015-08-19 22:12:06 -0700

    finished data structures sprint part 1.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
 
+Queue.prototype.enqueue = function(value){
+  var nextIndex = _.size(this.storage);
+  this.storage[nextIndex] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  var removed = this.storage[0];
+  for (var key in this.storage){
+    this.storage[key]=this.storage[Number(key)+1];
+  }
+  delete this.storage[_.size(this.storage)-1];
+  return removed;
+};
+
+Queue.prototype.size = function(){
+  return _.size(this.storage);
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  };
+
+
+
+Stack.prototype.push = function(value){
+  this.storage[_.size(this.storage)]=value;
+};
+Stack.prototype.pop = function(){
+  var lastInd = _.size(this.storage)-1;
+  var hold = this.storage[lastInd];
+  delete this.storage[lastInd];
+  return hold;
+};
+Stack.prototype.size = function(){
+  return _.size(this.storage);
 };
 
 
