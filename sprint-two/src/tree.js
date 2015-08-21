f8118aa2e798a4c17f2c70d6644b25dba81ecd81
jwareing <jwareing89@gmail.com>
2015-08-21 11:46:20 -0700

    fixed tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,26 +1,41 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree,treeMethods);
   return newTree;
 };
 
 
   // your code here
-  newTree.children = null;  // fix me
+  //newTree.children = null;  // fix me
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+  if (this.value === target){
+    return true;
+  }
+  var nodeSearcher = function(nodes){
+    return _.reduce(nodes, function(prev,node){
+      if (prev){
+        return true;
+      }
+      if (node.value === target){
+        return true;
+      }
+      if (node.children.length){
+        return nodeSearcher(node.children);
+      }
+      return false;
+    },false);
+  };
+  return nodeSearcher(this.children);
 };
 
 
