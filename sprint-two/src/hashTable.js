2f8c04806cdf240e4ebf67dd9344b19e8383d528
Manningham <Nstarun@gmail.com>
2015-08-22 10:02:19 -0700

    Started implementing hash table resizing for growth

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,39 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._insertCount = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._insertCount++;
+  if (this._insertCount >= (this._limit * (3/4))) {
+    var temp = [];
+    this._insertCount = 0;
+    this._limit = this._limit * 2;
+    this._storage.each(function(bucket){
+      if (bucket) {
+        _.each(bucket, function (keyValPair) {
+          if (keyValPair[1] !== null) {
+            temp.push(keyValPair);
+          }
+        });
+      }
+    });
+    this._storage = LimitedArray(this._limit);
+    var context = this;
+    _.each(temp, function(keyValPair) {
+      debugger
+      context.insert.call(context, keyValPair[0], keyValPair[1]);
+    });
+  }    
   if (this._storage.get(i) === undefined){
     this._storage.set(i, []);
   }
   var bucket = this._storage.get(i);
   bucket.push([k,v]);
   this._storage.set(i,bucket);
+  this._insertCount++;
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -31,6 +54,7 @@ HashTable.prototype.remove = function(k){
   _.each(bucket, function(tuple,index){
     if (tuple[0] === k){
       tuple[1] = null;
+      this._insertCount--;
     }
   });
 };
