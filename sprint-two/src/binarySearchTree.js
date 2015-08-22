e0eaf2ead30fe307536127b1bba12834b48cb551
jwareing <jwareing89@gmail.com>
2015-08-21 21:23:05 -0700

    Added all the other data structure algorithms

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,61 @@
 var BinarySearchTree = function(value){
+  var binTree = Object.create(BinarySearchTree.prototype);
+  binTree.value = value;
+  binTree.left = null;
+  binTree.right = null;
+  return binTree;
 };
 
 
+BinarySearchTree.prototype.insert = function(insertValue){
+  if (insertValue > this.value){
+    if (this.right !== null){
+      this.right.insert(insertValue);
+    }
+    else {
+      this.right = BinarySearchTree(insertValue);
+    }
+  }
+  else if (insertValue < this.value){
+    if (this.left !== null){
+      this.left.insert(insertValue);
+    }
+    else {
+      this.left = BinarySearchTree(insertValue);
+    }
+  }
+};
+BinarySearchTree.prototype.contains = function(searchVal){
+  // if searchVal === this.value return true
+  if (this.value === null) {
+    return false;
+  } else if (this.value === searchVal) {
+    return true;
+  }
+  // else if searchVal > this.value, search this.right
+  else if (searchVal > this.value && this.right !== null) {
+    return this.right.contains(searchVal);
+    //else if searchVal < this.value, search this.left
+  } else if (searchVal < this.value && this.left !== null) {
+    return this.left.contains(searchVal);
+  }
+  //return false
+  return false
+
+};
+BinarySearchTree.prototype.depthFirstLog = function(cAlLbzAckbIatch){
+  //run the callback on this.value
+  cAlLbzAckbIatch(this.value);
+
+  //if this.left is defined, run depthFirstLog on it
+  if (this.left) {
+    this.left.depthFirstLog(cAlLbzAckbIatch);
+  }
+  //if this.right is defined, run depthFirstLog on it
+  if (this.right) {
+    this.right.depthFirstLog(cAlLbzAckbIatch);
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,34 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(i) === undefined){
+    this._storage.set(i, []);
+  }
+  var bucket = this._storage.get(i);
+  bucket.push([k,v]);
+  this._storage.set(i,bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  var res;
+  _.each(bucket, function(tuple,index){
+    if (tuple[0] === k){
+      res = tuple[1];
+    }
+  });
+  return res;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  _.each(bucket, function(tuple,index){
+    if (tuple[0] === k){
+      tuple[1] = null;
+    }
+  });
 };
 
 
